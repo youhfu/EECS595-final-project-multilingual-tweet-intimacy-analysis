3aff97c440e4708819b96deda087b0ddc734f817
Youheng Fu <youhfu@umich.edu>
2024-05-02 01:07:36 -0400

    Add files via upload

---
+++ bash_profile/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=distil_bert
+#SBATCH --account=eecs595w24_class
+#SBATCH --partition=spgpu
+#SBATCH --gpus=1
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=16gb
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --output=distil_bert.out
+
+# The application(s) to execute along with its input arguments and options:
+
+/bin/hostname
+echo "job started"
+nvidia-smi
+python3 distil_bert.py
